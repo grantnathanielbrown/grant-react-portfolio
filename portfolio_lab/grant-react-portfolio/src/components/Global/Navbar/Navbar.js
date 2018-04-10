8cbcd5da35157daf21d8b5e12521549ac4b2d898
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-04-10 18:05:04 -0400

    more styling stuff

--- portfolio_lab/grant-react-portfolio/src/components/Global/Navbar/Navbar.js
+++ portfolio_lab/grant-react-portfolio/src/components/Global/Navbar/Navbar.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import { Route, Link, Redirect, Switch } from 'react-router-dom'
 class Navbar extends Component {
     linkVisit() {
-        var link = document.nav.childNodes
-        link.classList.toggle('highlight-link')
+        var butt = document.getElementsByClassName('navbar')
+        console.log(butt)
+        butt.classList.toggle('highlight-link')
     }
     render() {
         return (
             <div>
-                <nav className="navbar">
+                <nav className="navbar" onClick={this.linkVisit}>
                     <h1>GRANT BROWN</h1>
                     <Link to='/home'>HOME</Link>
                     <Link to='/projects'>PROJECTS</Link>
