36a99b9475e1cf38c22831ac45fe4a36172f4127
grantnathanielbrown <32802164+grantnathanielbrown@users.noreply.github.com>
2018-06-19 18:40:09 -0400

    Moved some stuff around

--- src/components/Pages/Projects/ProjectsPage/ProjectsPage.js
+++ src/components/Pages/Projects/ProjectsPage/ProjectsPage.js
@@ -11,14 +11,14 @@ class ProjectsPage extends Component {
                 <main className="project-container">
                     <ProjectLink title="MANGAD" description="An image sharing site for manga enthusiasts."
                      link="https://mangad.herokuapp.com/" image="https://ih1.redbubble.net/image.491015001.2844/flat,1000x1000,075,f.u1.jpg"/>
-                    <ProjectLink title="esGApe" description="A site to share details on local restaurants and bars."
-                     link="https://disillusioned-pet.surge.sh" image="https://cdn.shopify.com/s/files/1/0917/5854/products/CabSauv_WEB.jpg?v=1520424563"/>
-                    {/* <ProjectLink title="Bear Attacks!" description="Choose your own adventure style text games."
-                     link="https://github.com/grantnathanielbrown/snakestuff" image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/European_Brown_Bear.jpg/1200px-European_Brown_Bear.jpg"/> */}
                     <ProjectLink title="Slithereen" description="Multiplayer trivia game." link="https://grantnathanielbrown.github.io/slithereen/"
                     image="https://i.imgur.com/lS0M6kP.png" />
+                    {/* <ProjectLink title="Bear Attacks!" description="Choose your own adventure style text games."
+                     link="https://github.com/grantnathanielbrown/snakestuff" image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/European_Brown_Bear.jpg/1200px-European_Brown_Bear.jpg"/> */}
                     <ProjectLink title="Hangman" description="Replayable hangman game." link="https://grantnathanielbrown.github.io/wdi-project-one/"
                      image="https://images-na.ssl-images-amazon.com/images/I/81xt2%2BPD0IL.png" />
+                     <ProjectLink title="esGApe" description="A site to share details on local restaurants and bars."
+                      link="https://disillusioned-pet.surge.sh" image="https://cdn.shopify.com/s/files/1/0917/5854/products/CabSauv_WEB.jpg?v=1520424563"/>
                 </main>
             </div>
         );
