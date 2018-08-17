f878dd91e3ae076dd7fb948edb62f75ac35bff7b
grantnathanielbrown <32802164+grantnathanielbrown@users.noreply.github.com>
2018-08-17 14:24:31 -0400

    fixed resume link

--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -19,7 +19,7 @@ class HomePage extends Component {
                         <a href="https://github.com/grantnathanielbrown" ><i class="fab fa-github fa-2x"></i> GitHub</a>
                         <a href="https://www.linkedin.com/in/grant-brown-a6b87a92/"><i class="fab fa-linkedin fa-2x"></i> LinkedIn</a>
                         <a href="mailto:gnb225@nyu.edu" ><i class="fas fa-envelope-square fa-2x"></i> Email (gnb225@nyu.edu)</a>
-                        <a href="https://github.com/grantnathanielbrown/grant-brown-resume/raw/master/Grant%20Brown%20Resume%206.19.2018.pdf"><i class="fas fa-file-alt fa-2x"></i> Resume</a>
+                        <a href="https://github.com/grantnathanielbrown/grant-brown-resume/blob/master/Grant%20Brown%20Resume%207.30.2018.pdf"><i class="fas fa-file-alt fa-2x"></i> Resume</a>
                         <a href="tel:571-249-8475" ><i class="fas fa-phone-square fa-2x"></i> 571-249-8475</a>
                     </div>
                 </div>
