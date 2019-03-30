118d6e97e9f70a61d009673c74dba854a1b40fe8
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-03-30 18:36:46 -0400

    animation_in_progress

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 
 ## TODO
 
+- more interesting effect on the navigation bar
+- color-coded technologies for projects
+- white out for project images
+- blog post
+- fix blog for mobile
+
+
+
+- color scheme randomizer?
+- add extra projects?
+- hamburg navigation mobile?
--- src/components/Global/Navbar/Navbar.js
+++ src/components/Global/Navbar/Navbar.js
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
 // dependencies
 import { Route, Link, Redirect, Switch } from 'react-router-dom';
-class Navbar extends Component {
-    // componentWillMount() {
-    //     function mobileCompressor(x) {
-    //         if (x.matches) { // If media query matches
-    //             // var element = document.getElementById("navbar-main");
-    //             // element.className.add('mobile');
-    //             var navClass = 'navbar mobile';
-    //             console.log(navClass);
+// import Anime from 'react-anime';
+import anime from 'animejs';
 
-    //         } else {
-    //             var navClass = 'navbar';
-    //             console.log(navClass);
-    //         }
-    //     }
-        
-    //     var x = window.matchMedia("(max-width: 700px)");
-    //     mobileCompressor(x);
+class Navbar extends Component {
+    constructor(props) {
+        super(props) 
+
+
+        this.buttonAnimation = this.buttonAnimation.bind(this);
+        this.navHover = this.navHover.bind(this);
+        this.navAway = this.navAway.bind(this);
+    }
+    buttonAnimation(element, color, direction){
+        anime({
+            
+            targets: element,
+            // background: rgb(0, 0, 150),
+            color: color,
+            duration: 2000,
+            direction: direction
+        })
+    } 
+
+    // test(){
+    //     anime({
+    //         translateX: 250
+    //     })
     // }
+
+    navHover(e) {
+        var x = e.target;
+        console.log(x);
+        // this.buttonAnimation(x, '#1DA1F2','forwards');
+    }
+    navAway(e) {
+        var x = e.target;
+        console.log(x);
+        this.buttonAnimation(x, '#1DA1F2', 'reverse');
+    }
+
+    // navAway(e) {
+    //     var element = e.target;
+    //     console.log(element);
+    //     anime({
+            
+    //         targets: element,
+    //         // background: rgb(0, 0, 150),
+    //         color: "#000000",
+    //         duration: 1000,
+    //     })
+    // }
+
     render() {
         return (
             <div>
-                <nav className="navbar" id="navbar-main">
+                <nav className="navbar" id="navbar-main">                
                     <h1>GRANT BROWN</h1>
-                    <Link to='/'>HOME</Link>
-                    <Link to='/projects'>PROJECTS</Link>
-                    <Link to='/blog'>BLOG</Link>
+                    {/* <Anime> */}
+
+                        <Link className="nav-anchor" onMouseOver={this.navHover} onMouseLeave={this.navAway}  to='/'>HOME</Link>
+                        <Link className="nav-anchor" onMouseOver={this.navHover} onMouseLeave={this.navAway}  to='/projects'>PROJECTS</Link>
+                        <Link className="nav-anchor" onMouseOver={this.navHover} onMouseLeave={this.navAway}  to='/blog'>BLOG</Link>
+                    {/* </Anime> */}
                 </nav>
             </div>
         )
