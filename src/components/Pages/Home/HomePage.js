7d1b3471d3330cb6917cf9b91e7afd7e818075ab
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-04-03 00:30:44 -0400

    svg_rendering_successfully

--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -6,8 +6,13 @@ class HomePage extends Component {
         I enjoy building websites and applications that give users a tighly curated experience,
         focusing on functionality over fluff. Regardless of whether I'm the student or teacher,
         I'm learning, and loving it.`
+        let test = <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" x="0px" y="0px" viewBox="0 0 100 125"><defs><g id="a"><path class="path" fill="#ffffff" stroke="#000000" stroke-width="4" d=" M 51.3 22.9 Q 53 21.55 57.75 21.55 L 57.75 18.85 Q 54.35 18.85 52.3 19.5 49.95 20.2 48.6 21.9 47.25 23.9 47.25 28.3 L 47.25 42.9 Q 46.95 46 42 50 42.45 50.05 44.95 52.95 47.45 55.85 47.25 57.1 L 47.25 72 Q 47.25 76.4 48.6 78.45 51.3 81.15 58.05 81.15 L 58.05 78.45 Q 53.3 78.45 51.65 77.1 50.3 75.75 50.3 72 L 50.3 57.1 Q 50.3 53.4 46.25 50.05 46.5 49.95 48.25 47.95 50.3 45.6 50.3 42.9 L 50.3 28.3 Q 50.3 24.25 51.3 22.9 Z"/></g></defs><g transform="matrix( 1, 0, 0, 1, 0,0) "><use xlinkHref="#a"/></g></svg>
+        
         return (
             <div className="home-page">
+                {/* <div className="test-svg">
+                    {test}
+                </div> */}
                 <SubHeader img={"https://i.imgur.com/1Ox1DcB.jpg"} header={"Grant Brown"} />
                 <div className="about-contact-columns">
                     <div className="about-me">
