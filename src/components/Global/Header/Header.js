9e050cb346d2e1f91e30f513e21acfbaf2e1ad00
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-06-13 16:28:05 -0400

    anna may react integration package does not work

--- src/components/Global/Header/Header.js
+++ src/components/Global/Header/Header.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import Anime from 'react-anime';
 
 class Header extends Component {
     render(props) {
         return (
-            <Anime opacity={0}>
                 <header>
                     <img src={this.props.img} />
                     <h1>{this.props.header}</h1>
                     <h3>{this.props.footnote}</h3>
                 </header>
-            </Anime>
         );
     }
 }
